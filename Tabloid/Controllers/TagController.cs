b4332bafd7221d6d055968352e7470c733214c9d
Drew Kirkland <drew.kirkland@gmail.com>
2020-07-09 15:54:35 -0400

    Model, repository, and controller have all been setup for Tag feature.

---
+++ Tabloid/Controllers/TagController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Data;
+using Tabloid.Models;
+using Tabloid.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly TagRepository _tagRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+
+        public TagController(ApplicationDbContext context)
+        {
+            _tagRepository = new TagRepository(context);
+            _userProfileRepository = new UserProfileRepository(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tagRepository.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var tag  = _tagRepository.GetById(id);
+            if (tag != null)
+            {
+                NotFound();
+            }
+            return Ok(tag);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Tag tag)
+        {
+         
+            _tagRepository.Add(tag);
+            return CreatedAtAction(nameof(Get), new { id = tag.Id }, tag);
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+
+    }
+}
