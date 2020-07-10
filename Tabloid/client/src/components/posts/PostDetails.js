8bb272ff4219f14349e2172fbadf49d3ab97f7a0
Jon <durrjp1@gmail.com>
2020-07-10 12:01:08 -0500

    post details styling

--- Tabloid/client/src/components/posts/PostDetails.js
+++ Tabloid/client/src/components/posts/PostDetails.js
@@ -1,34 +1,36 @@
 import React, { useContext, useEffect, useState } from "react"
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle
-  } from 'reactstrap';
 import { PostContext } from "../../providers/PostProvider";
 import { useParams } from "react-router-dom";
+import "./PostDetails.css"
+import moment from "moment";
 
 const PostDetails = () => {
     const {getPostById} = useContext(PostContext);
     const [onePost, setOnePost] = useState();
     const { id } = useParams();
-    
+
     useEffect(() => {
         getPostById(id).then(setOnePost)
-      }, []);
+    }, []);
 
+      
     if (!onePost) {
-    return null;
+        return null;
     }
+
+    const formattedDate = moment().format('MM/DD/YYYY', onePost.publishDateTime)
 
     return (
         <>
-            <Card>
-                <CardImg top width="100%" src={onePost.imageLocation} alt="Card image cap" />
-                <CardBody>
-                <CardTitle>{onePost.title}</CardTitle>
-                <CardSubtitle>Written by: {onePost.userProfile.displayName}</CardSubtitle>
-                <CardText>{onePost.content}</CardText>
-                </CardBody>
-            </Card>
+            <div className="postDetailsContainer">
+                <div className="imgContainer">
+                    <img src={onePost.imageLocation} alt="" />
+                </div>
+                <div className="titleContainer"><h1>{onePost.title}</h1></div>
+                <div className="authorContainer">Written by: <span className="author">{onePost.userProfile.displayName}</span></div>
+                <div className="contentContainer">{onePost.content}</div>
+                <div className="publishedDate">Published: {formattedDate}</div>
+            </div>
         </>
 
     )
