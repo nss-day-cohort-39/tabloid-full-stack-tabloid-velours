fd00a25eb22ad129fe1c4711ea4583e255917b9b
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-07-14 13:03:54 -0500

    deactivate

--- Tabloid/client/src/components/comments/Comment.js
+++ Tabloid/client/src/components/comments/Comment.js
@@ -4,59 +4,75 @@ import {
   ListGroupItem,
   ListGroupItemHeading,
   ListGroupItemText,
-  Modal, ModalHeader, ModalBody
+  Modal,
+  ModalHeader,
+  ModalBody,
 } from "reactstrap";
-import { CommentContext } from "../../providers/CommentProvider"
+import { CommentContext } from "../../providers/CommentProvider";
 import moment from "moment";
 import { EditComment } from "./EditComment";
 
-
 export const Comment = ({ comment }) => {
-  const { deleteComment} = useContext(CommentContext);
-  const formattedDate = moment(comment.createDateTime).format("MM/DD/YYYY")
+  const { deleteComment } = useContext(CommentContext);
+  const formattedDate = moment(comment.createDateTime).format("MM/DD/YYYY");
   const userProfile = JSON.parse(sessionStorage.getItem("userProfile"));
-  const [modal, setModal] = useState(false)
-  const toggleModal = () => setModal(!modal) 
-  
-      return (
-      <>
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => setModal(!modal);
+
+  return (
+    <>
       <br></br>
-        <ListGroup>
-          <ListGroupItem active>
-            <ListGroupItemHeading>
-              {comment.subject}
-            </ListGroupItemHeading>
-            <ListGroupItemText>
-              "{comment.content}"
-              <br></br>
-              <i>posted by {comment.userProfile.fullName} on {formattedDate}</i>
-            </ListGroupItemText>
-            {userProfile.id === comment.userProfileId  &&
-            <i className="fa fa-pencil-square-o" aria-hidden="true" onClick={() => toggleModal() }>
-                Edit
-            </i>  
-}
-            <br></br>
-            {Boolean(userProfile.id === comment.userProfileId | userProfile.userTypeId === 1) &&
-            
-  <i className="fa fa-trash-o" aria-hidden="true" onClick={() =>
-    window.confirm("Are you sure you wish to delete this comment?") && deleteComment(comment.id)}>
-    Delete
-  </i> 
-            }
-            <br></br>
-          </ListGroupItem>
-        </ListGroup>
+      <ListGroup>
+        <ListGroupItem active>
+          <ListGroupItemHeading>{comment.subject}</ListGroupItemHeading>
+          <ListGroupItemText>
+            "{comment.content}"<br></br>
+            <i>
+              posted by {comment.userProfile.fullName} on {formattedDate}
+            </i>
+          </ListGroupItemText>
+          {userProfile.id === comment.userProfileId && (
+            <i
+              className="fa fa-pencil-square-o"
+              aria-hidden="true"
+              onClick={() => toggleModal()}
+            >
+              Edit
+            </i>
+          )}
+          <br></br>
+          {Boolean(
+            (userProfile.id === comment.userProfileId) |
+              (userProfile.userTypeId === 1)
+          ) && (
+            <i
+              className="fa fa-trash-o"
+              aria-hidden="true"
+              onClick={() =>
+                window.confirm(
+                  "Are you sure you wish to delete this comment?"
+                ) && deleteComment(comment.id)
+              }
+            >
+              Delete
+            </i>
+          )}
+          <br></br>
+        </ListGroupItem>
+      </ListGroup>
 
-
-        <Modal isOpen={modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
-                        toggle={toggleModal} contentClassName="custom-modal-style-product" >
-                        <ModalHeader toggle={toggleModal}>Edit "{comment.subject}"</ModalHeader>
-                        <ModalBody>
-                            <EditComment comment={comment} toggle={toggleModal} />
-                        </ModalBody>
-                    </Modal>
-      </>
-    );
- 
+      <Modal
+        isOpen={modal}
+        modalTransition={{ timeout: 700 }}
+        backdropTransition={{ timeout: 1300 }}
+        toggle={toggleModal}
+        contentClassName="custom-modal-style-product"
+      >
+        <ModalHeader toggle={toggleModal}>Edit "{comment.subject}"</ModalHeader>
+        <ModalBody>
+          <EditComment comment={comment} toggle={toggleModal} />
+        </ModalBody>
+      </Modal>
+    </>
+  );
 };
--- Tabloid/client/src/components/userProfiles/UserProfile.js
+++ Tabloid/client/src/components/userProfiles/UserProfile.js
@@ -1,67 +1,89 @@
 import React, { useContext, useState } from "react";
-import { ListGroupItem, ListGroup, Modal, ModalHeader, ModalBody
+import {
+  ListGroupItem,
+  ListGroup,
+  Modal,
+  ModalHeader,
+  ModalBody,
 } from "reactstrap";
 import "./UserProfile.css";
 import { useHistory } from "react-router-dom";
 import { UserProfileContext } from "../../providers/UserProfileProvider";
-import {EditUserType} from "./EditUserType"
+import { EditUserType } from "./EditUserType";
+import { UserProfileList } from "./UserProfileList";
 
 export const UserProfile = ({ userProfile }) => {
   const { editUserProfile } = useContext(UserProfileContext);
-  const [modal, setModal] = useState(false)
-  const toggleModal = () => setModal(!modal) 
+  const [modal, setModal] = useState(false);
+  const toggleModal = () => setModal(!modal);
   const history = useHistory();
   const handleClick = () => {
     history.push(`/userprofiles/${userProfile.id}`);
   };
 
-  const deActivate = () => {};
+  const deActivate = () => {
+    window.confirm("Are you sure you wish to deactivate this user?") &&
+      editUserProfile({
+        id: userProfile.id,
+        firebaserUserId: userProfile.firebaserUserId,
+        displayName: userProfile.displayName,
+        firstName: userProfile.firstName,
+        lastName: userProfile.lastName,
+        email: userProfile.email,
+        createDateTime: userProfile.createDateTime,
+        imageLocation: userProfile.imageLocation,
+        userTypeId: userProfile.userTypeId,
+        IsActivated: 0,
+      });
+  };
 
-  return (
-    <>
-      <ListGroupItem>
-        <ListGroup horizontal className="userProfile">
-          <div className="user--info">{userProfile.fullName}</div>
-          <div className="user--info">{userProfile.displayName}</div>
-          <div className="user--info">{userProfile.userType.name}</div>
-          <ListGroup horizontal>
-            <div className="icon--userProfile">
-              <i
-                onClick={handleClick}
-                className="fa fa-info-circle"
-                aria-hidden="true"
-              ></i>
-            </div>
-            <div className="icon--userProfile">
-              <i
-                onClick={deActivate}
-                className="fa fa-window-close-o"
-                aria-hidden="true"
-              ></i>
-            </div>
-            <div className="icon--userProfile">
-              <i
-                onClick={() => toggleModal()}
-                className="fa fa-pencil-square-o"
-                aria-hidden="true"
-              ></i>
-            </div>
+  if (userProfile.IsActivated === true) {
+    return (
+      <>
+        <ListGroupItem>
+          <ListGroup horizontal className="userProfile">
+            <div className="user--info">{userProfile.fullName}</div>
+            <div className="user--info">{userProfile.displayName}</div>
+            <div className="user--info">{userProfile.userType.name}</div>
+            <ListGroup horizontal>
+              <div className="icon--userProfile">
+                <i
+                  onClick={handleClick}
+                  className="fa fa-info-circle"
+                  aria-hidden="true"
+                ></i>
+              </div>
+              <div className="icon--userProfile">
+                <i
+                  onClick={deActivate}
+                  className="fa fa-window-close-o"
+                  aria-hidden="true"
+                ></i>
+              </div>
+              <div className="icon--userProfile">
+                <i
+                  onClick={() => toggleModal()}
+                  className="fa fa-pencil-square-o"
+                  aria-hidden="true"
+                ></i>
+              </div>
+            </ListGroup>
           </ListGroup>
-        </ListGroup>
-      </ListGroupItem>
+        </ListGroupItem>
 
-      <Modal
-        isOpen={modal}
-        modalTransition={{ timeout: 700 }}
-        backdropTransition={{ timeout: 1300 }}
-        toggle={toggleModal}
-        contentClassName="custom-modal-style-product"
-      >
-        <ModalHeader toggle={toggleModal}>Edit User Type</ModalHeader>
-        <ModalBody>
-          <EditUserType userProfile={userProfile} toggle={toggleModal} />
-        </ModalBody>
-      </Modal>
-    </>
-  );
+        <Modal
+          isOpen={modal}
+          modalTransition={{ timeout: 700 }}
+          backdropTransition={{ timeout: 1300 }}
+          toggle={toggleModal}
+          contentClassName="custom-modal-style-product"
+        >
+          <ModalHeader toggle={toggleModal}>Edit User Type</ModalHeader>
+          <ModalBody>
+            <EditUserType userProfile={userProfile} toggle={toggleModal} />
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
 };
