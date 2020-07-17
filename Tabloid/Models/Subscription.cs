525f12902869422abecc9761c3334a19cfae66a6
Drew Kirkland <drew.kirkland@gmail.com>
2020-07-17 12:10:26 -0400

    Setup Subscription model.

---
+++ Tabloid/Models/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Subscription
+    {
+        public int id { get; set; }
+        public int SubscriberUserProfileId { get; set; }
+        public int AuthorUserProfileId { get; set; }
+        public DateTime BeginDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+
+    }
+}
