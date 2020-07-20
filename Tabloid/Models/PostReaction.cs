6313d9277f1e8892144b27e60c577df2125888dd
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-07-20 10:07:41 -0500

    before pr

--- Tabloid/Models/PostReaction.cs
+++ Tabloid/Models/PostReaction.cs
@@ -10,8 +10,11 @@ namespace Tabloid.Models
         public int Id { get; set; }
         public int PostId { get; set; }
         public int ReactionId { get; set; }
+        public int UserProfileId { get; set; }
         public Post Post { get; set; }
         public Reaction Reaction { get; set; }
+        public UserProfile UserProfile { get; set; }
+
 
     }
 }
